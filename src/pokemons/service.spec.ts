ef4ac6adc2287163ffc6128479b18da29a9172a4
Samuel Maina <mainasamkaranja@gmail.com>
2022-06-01 15:08:09 +0300

    Implemented CRUD tests for Pokemon Service

--- src/pokemons/service.spec.ts
+++ src/pokemons/service.spec.ts
@@ -6,6 +6,48 @@ describe('PokemonService', () => {
   beforeEach(() => {});
 
   test('getAll', () => {
-    // expect().toEqual();
+    expect(pokemonService.getAll().length).toEqual(pokemons.length);
+  });
+
+  test('getOne happy path', () => {
+    const pokemon = pokemonService.getOne(1);
+    expect(pokemon.name).toContain('Bulbizarre');
+  });
+
+  test('getOne sad path', () => {
+    expect(() => {
+      pokemonService.getOne(100);
+    }).toThrowError('Pokemon not found');
+  });
+
+  test('create pokemon', () => {
+    const data = {
+      name: 'Samuel',
+      hp: 100,
+      cp: 100,
+      picture: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      types: ['Poison'],
+    };
+    expect(pokemons.length).toBe(10);
+    pokemonService.create(data);
+    expect(pokemons.length).toBe(11);
+  });
+
+  test('delete pokemon happy path', () => {
+    expect(pokemons.length).toBe(11);
+
+    const deletedPokemon = pokemonService.deleteOne(1);
+    expect(pokemons.length).toBe(10);
+
+    // make sure that deleted pokemon cannot be found.
+    expect(() => {
+      pokemonService.getOne(deletedPokemon.id);
+    }).toThrowError('Pokemon not found');
+  });
+
+  test('delete pokemon sad path', () => {
+    expect(() => {
+      pokemonService.deleteOne(100);
+    }).toThrowError('Could not delete Pokemon');
   });
 });
--- src/pokemons/service.ts
+++ src/pokemons/service.ts
@@ -3,19 +3,38 @@ import { Pokemon } from './model';
 
 export class PokemonService {
   getAll() {
-
+    return pokemons;
   }
-  getOne(id: number) {
 
-  }
-  create(pokemon: Pokemon) {
+  getOne(id: number): Pokemon {
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+    if (!pokemon) {
+      throw Error('Pokemon not found');
+    }
 
+    return pokemon;
   }
+
+  create(pokemon: Omit<Pokemon, 'id'>): Pokemon {
+    pokemons.push({ ...pokemon, id: pokemons.length + 1 });
+
+    return pokemons[pokemons.length - 1];
+  }
+
   update(id: number, data: Partial<Omit<Pokemon, 'id'>>) {
+    const pokemonIndex = pokemons.findIndex((pokemon) => pokemon.id === id);
+    pokemons[pokemonIndex] = { ...pokemons[pokemonIndex], ...data };
 
+    return pokemons[pokemonIndex];
   }
-  deleteOne(id: number) {
 
+  deleteOne(id: number): Pokemon {
+    const pokemonIndex = pokemons.findIndex((pokemon) => pokemon.id === id);
+    if (pokemonIndex < 0) {
+      throw Error('Could not delete Pokemon');
+    }
+
+    return pokemons.splice(pokemonIndex, 1)[0];
   }
 }
 export default new PokemonService();
